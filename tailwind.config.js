b91ed49b6c291acfaab908346cdd795b2ce662f4
Tanya Agarwal <135453589+tanya13agarwal@users.noreply.github.com>
2024-06-03 18:26:19 +0530

    added required helper files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors : {
+      white : "#FFFFFF",
+      btnRed: "#FF3147",
+      black : "#000000",
+      grey : "#808080",
+      bgDull : "#FAFAFA",
+      greyDull : "#808080",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
